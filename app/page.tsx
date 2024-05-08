5057e7a5203ed9dc4d4da698d275d798459f3f7e
Hestranha <94768439+Hestranha@users.noreply.github.com>
2024-05-07 20:55:39 -0500

    terminado creo xd

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import DescargarTitok from "./ui/tiktok/descargarTiktok";
 const Page: React.FC = () => {
 
   return (
-    <main className="flex flex-col justify-center items-center min-h-screen p-6 gap-2 bg-[#ccc] w-full">
+    <main className="flex flex-col justify-center items-center min-h-screen py-6 gap-2 bg-[#ccc] w-full">
       <DescargarInstagram />
       <DescargarTitok />
     </main>
