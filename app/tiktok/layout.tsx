e3a426d54dfb45b94142ce8adeba8575e551982f
Hestranha <94768439+Hestranha@users.noreply.github.com>
2024-05-07 20:55:02 -0500

    terminado creo xd

--- app/tiktok/layout.tsx
+++ app/tiktok/layout.tsx
@@ -1,7 +1,7 @@
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex items-center min-h-screen flex-col gap-2 p-6 w-full" style={{ background: 'radial-gradient(circle, #1f1f1f 40%, black)' }}>
+    <main className="flex items-center min-h-screen flex-col gap-2 py-6 w-full" style={{ background: 'radial-gradient(circle, #1f1f1f 40%, black)' }}>
       {children}
     </main>
   );
