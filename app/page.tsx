2bb384a475fdce8bf329509d0fbfc178dfc0e604
Hestranha <94768439+Hestranha@users.noreply.github.com>
2024-05-07 21:06:11 -0500

    terminado creo 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import DescargarInstagram from "./ui/instagram/descargar";
 import DescargarTitok from "./ui/tiktok/descargarTiktok";
+import Link from "next/link";
 
 const Page: React.FC = () => {
 
   return (
     <main className="flex flex-col justify-center items-center min-h-screen py-6 gap-2 bg-[#ccc] w-full">
+      <Link href="/instagram" className="py-2 px-4 rounded-md text-white bg-purple-800 hover:bg-purple-900 transition-colors duration-300">
+        Ir a principal
+      </Link>
       <DescargarInstagram />
+      <Link href="/tiktok" className="py-2 px-4 rounded-md text-white bg-neutral-800 hover:bg-neutral-900 transition-colors duration-300">
+        Ir a principal
+      </Link>
       <DescargarTitok />
     </main>
   );
