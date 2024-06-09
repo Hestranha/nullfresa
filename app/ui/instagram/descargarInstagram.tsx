eb0c002368f06831e5334955d97e7d606838f49c
Hestranha <94768439+Hestranha@users.noreply.github.com>
2024-06-08 20:45:51 -0500

    ultimos ajustes?

--- app/ui/instagram/descargarInstagram.tsx
+++ app/ui/instagram/descargarInstagram.tsx
@@ -88,14 +88,9 @@ export default function DescargarInstagram() {
                             style={{ cursor: loading ? "not-allowed" : "pointer" }}
                         >
                             {loading ? (
-                                <React.Fragment>
-                                    <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" style={{ shapeRendering: "auto", display: "block", background: "transparent" }}><g><circle stroke-linecap="round" fill="none" stroke-dasharray="50.26548245743669 50.26548245743669" stroke="#ffffff" strokeWidth="8" r="32" cy="50" cx="50">
-                                        <animateTransform values="0 50 50;360 50 50" keyTimes="0;1" dur="1s" repeatCount="indefinite" type="rotate" attributeName="transform" />
-                                    </circle><g /></g></svg>
-                                    <p>
-                                        BUSCANDO
-                                    </p>
-                                </React.Fragment>
+                                <svg className="w-7 h-7" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" style={{ shapeRendering: "auto", display: "block", background: "transparent" }}><g><circle strokeLinecap="round" fill="none" strokeDasharray="50.26548245743669 50.26548245743669" stroke="#ffffff" strokeWidth="8" r="32" cy="50" cx="50">
+                                    <animateTransform values="0 50 50;360 50 50" keyTimes="0;1" dur="1s" repeatCount="indefinite" type="rotate" attributeName="transform" />
+                                </circle><g /></g></svg>
                             ) : (
                                 <p className="font-bold text-sm tracking-widest">
                                     BUSCAR
