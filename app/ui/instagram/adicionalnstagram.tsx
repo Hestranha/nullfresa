b0f9904fabff93628d92e67d1d7d2d756e92e9f5
Hestranha <94768439+Hestranha@users.noreply.github.com>
2024-07-20 19:32:44 -0500

    obtener info

--- app/ui/instagram/adicionalnstagram.tsx
+++ app/ui/instagram/adicionalnstagram.tsx
@@ -260,7 +260,7 @@ export default function AdicionalInstagram() {
                             onClick={() => setUsername((inputUsuario as any).current.value)}
                         >
                             {loading ? (
-                                <svg className="w-7 h-7" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" style={{ shapeRendering: "auto", display: "block", background: "transparent" }}><g><circle strokeLinecap="round" fill="none" strokeDasharray="50.26548245743669 50.26548245743669" stroke="#ffffff" strokeWidth="8" r="32" cy="50" cx="50">
+                                <svg className="w-5 h-5 lg:w-7 lg:h-7" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" style={{ shapeRendering: "auto", display: "block", background: "transparent" }}><g><circle strokeLinecap="round" fill="none" strokeDasharray="50.26548245743669 50.26548245743669" stroke="#ffffff" strokeWidth="8" r="32" cy="50" cx="50">
                                     <animateTransform values="0 50 50;360 50 50" keyTimes="0;1" dur="1s" repeatCount="indefinite" type="rotate" attributeName="transform" />
                                 </circle><g /></g></svg>
                             ) : (
